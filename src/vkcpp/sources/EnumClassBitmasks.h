255550a7fbf1d8e183c10c762fe66ad69caccdc3
Corentin Wallez <corentin@wallez.net>
2016-04-11 23:13:41 -0400

    vkcpp: make bitmasks bool convertible and add test

--- src/vkcpp/sources/EnumClassBitmasks.h
+++ src/vkcpp/sources/EnumClassBitmasks.h
@@ -39,39 +39,99 @@ namespace vk {
         static constexpr bool enable = false;
     };
 
-    template<typename T, typename = std::enable_if<IsVkBitmask<T>::enable>>
-    constexpr T operator | (T l, T r) {
+    template<typename T, typename Enable = void>
+    struct LowerBitmask {
+        static constexpr bool enable = false;
+    };
+
+    template<typename T>
+    struct LowerBitmask<T, typename std::enable_if<IsVkBitmask<T>::enable>::type> {
+        static constexpr bool enable = true;
+        using type = T;
+        static T Lower(T t) {return t;}
+    };
+
+    template<typename T>
+    struct BoolConvertible {
+        using base_type = T;
         using integral = typename std::underlying_type<T>::type;
-        return static_cast<T>(static_cast<integral>(l) | static_cast<integral>(r));
+
+        BoolConvertible(integral value) : value(value) {}
+        operator bool() const {return value != 0;}
+        operator T() const {return static_cast<T>(value);}
+
+        integral value;
+    };
+
+    template<typename T>
+    struct LowerBitmask<BoolConvertible<T>> {
+        static constexpr bool enable = true;
+        using type = T;
+        static type Lower(BoolConvertible<T> t) {return t;}
+    };
+
+    template<typename T1, typename T2, typename = typename std::enable_if<
+        LowerBitmask<T1>::enable && LowerBitmask<T2>::enable
+    >::type>
+    constexpr BoolConvertible<typename LowerBitmask<T1>::type>
+    operator | (T1 left, T2 right) {
+        using T = typename LowerBitmask<T1>::type;
+        using integral = typename std::underlying_type<T>::type;
+        T l = LowerBitmask<T1>::Lower(left);
+        T r = LowerBitmask<T1>::Lower(right);
+        return static_cast<integral>(l) | static_cast<integral>(r);
     }
-    template<typename T, typename = std::enable_if<IsVkBitmask<T>::enable>>
-    constexpr T operator & (T l, T r) {
+    template<typename T1, typename T2, typename = typename std::enable_if<
+        LowerBitmask<T1>::enable && LowerBitmask<T2>::enable
+    >::type>
+    constexpr BoolConvertible<typename LowerBitmask<T1>::type>
+    operator & (T1 left, T2 right) {
+        using T = typename LowerBitmask<T1>::type;
         using integral = typename std::underlying_type<T>::type;
-        return static_cast<T>(static_cast<integral>(l) & static_cast<integral>(r));
+        T l = LowerBitmask<T1>::Lower(left);
+        T r = LowerBitmask<T1>::Lower(right);
+        return static_cast<integral>(l) & static_cast<integral>(r);
     }
-    template<typename T, typename = std::enable_if<IsVkBitmask<T>::enable>>
-    constexpr T operator ^ (T l, T r) {
+    template<typename T1, typename T2, typename = typename std::enable_if<
+        LowerBitmask<T1>::enable && LowerBitmask<T2>::enable
+    >::type>
+    constexpr BoolConvertible<typename LowerBitmask<T1>::type>
+    operator ^ (T1 left, T2 right) {
+        using T = typename LowerBitmask<T1>::type;
         using integral = typename std::underlying_type<T>::type;
-        return static_cast<T>(static_cast<integral>(l) ^ static_cast<integral>(r));
+        T l = LowerBitmask<T1>::Lower(left);
+        T r = LowerBitmask<T1>::Lower(right);
+        return static_cast<integral>(l) ^ static_cast<integral>(r);
     }
-    template<typename T, typename = std::enable_if<IsVkBitmask<T>::enable>>
-    constexpr T operator ~ (T t) {
+    template<typename T1>
+    constexpr BoolConvertible<typename LowerBitmask<T1>::type>
+    operator ~ (T1 t) {
+        using T = typename LowerBitmask<T1>::type;
         using integral = typename std::underlying_type<T>::type;
-        return static_cast<T>(~static_cast<integral>(t));
+        return ~static_cast<integral>(LowerBitmask<T1>::Lower(t));
     }
 
-    template<typename T, typename = std::enable_if<IsVkBitmask<T>::enable>>
-    T& operator &= (T& l, T r) {
+    template<typename T, typename T2, typename = typename std::enable_if<
+        IsVkBitmask<T>::enable && LowerBitmask<T2>::enable
+    >::type>
+    T& operator &= (T& l, T2 right) {
+        T r = LowerBitmask<T2>::Lower(right);
         l = l & r;
         return l;
     }
-    template<typename T, typename = std::enable_if<IsVkBitmask<T>::enable>>
-    T& operator |= (T& l, T r) {
+    template<typename T, typename T2, typename = typename std::enable_if<
+        IsVkBitmask<T>::enable && LowerBitmask<T2>::enable
+    >::type>
+    T& operator |= (T& l, T2 right) {
+        T r = LowerBitmask<T2>::Lower(right);
         l = l | r;
         return l;
     }
-    template<typename T, typename = std::enable_if<IsVkBitmask<T>::enable>>
-    T& operator ^= (T& l, T r) {
+    template<typename T, typename T2, typename = typename std::enable_if<
+        IsVkBitmask<T>::enable && LowerBitmask<T2>::enable
+    >::type>
+    T& operator ^= (T& l, T2 right) {
+        T r = LowerBitmask<T2>::Lower(right);
         l = l ^ r;
         return l;
     }
